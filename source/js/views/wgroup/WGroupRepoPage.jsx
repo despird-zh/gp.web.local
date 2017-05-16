7aa34529b35fdfd7a64b76d9c56519e558ffc403
despird-zh <despird.zhang@yahoo.com>
2017-05-16 16:30:08 +0800

    workgroup repository support

--- source/js/views/wgroup/WGroupRepoPage.jsx
+++ source/js/views/wgroup/WGroupRepoPage.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ActDescription from 'material-ui/svg-icons/action/description';
+import ActSearch from 'material-ui/svg-icons/action/search';
+import CtntClear from 'material-ui/svg-icons/content/clear';
 import IconButton from 'material-ui/IconButton';
 import FileFolderOpen from 'material-ui/svg-icons/file/folder-open';
 import NaviExpandMore from 'material-ui/svg-icons/navigation/expand-more';
 import NaviExpandLess from 'material-ui/svg-icons/navigation/expand-less';
 import Popover from 'material-ui/Popover';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import Divider from 'material-ui/Divider';
+import MenuItem from 'material-ui/MenuItem';
 import {
   Table,
   TableBody,
@@ -27,11 +34,23 @@ function getStyles(muiTheme) {
       position: 'relative',
       marginTop: 10,
     },
-    topBar: {
+    topbar: {
       display: 'flex',
+      paddingLeft: 10,
+      position: 'relative',
     },
-    filter: {
+    spacer: { flex: 1 },
+    filterSearch: {
       marginRight: baseTheme.spacing.desktopGutterLess,
+      width: 150,
+    },
+    filterType: {
+      marginRight: baseTheme.spacing.desktopGutterLess,
+      width: 150,
+    },
+    filterClass: {
+      marginRight: baseTheme.spacing.desktopGutterLess,
+      width: 150,
     },
     leftPanel: {
       flex: 1,
@@ -154,7 +173,38 @@ class WGroupRepoPage extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { profileExpand: false, openRepoTree: false };
+    this.state = { 
+      profileExpand: false, 
+      openRepoTree: false,
+      showMoreFilter: false,
+      selectedRows:[],
+      rows: [{
+        id: '1',
+        name: 'hallo',
+        label: 'label'
+      },{
+        id: '2',
+        name: 'hallo',
+        label: 'label'
+      },{
+        id: '3',
+        name: 'hallo',
+        label: 'label'
+      },{
+        id: '4',
+        name: 'hallo',
+        label: 'label'
+      },{
+        id: '6',
+        name: 'hallo',
+        label: 'label'
+      },{
+        id: '5',
+        name: 'hallo',
+        label: 'label'
+      },
+      ]
+    };
     this.styles = getStyles(props.muiTheme);
   }
 
@@ -198,12 +248,46 @@ class WGroupRepoPage extends React.Component {
     }
   }
 
-  handJumpLink = (linkItem) => {
+  handleShowMoreFilter = () => {
+    this.setState({ showMoreFilter: !this.state.showMoreFilter });
+  }
+
+  handleJumpLink = (linkItem) => {
     console.log(linkItem)
   }
+
+  handleRowSelection = (selrows) => {
+    let _selectedRows = [];
+    if( (typeof selrows === 'string') && selrows === 'all'){
+      _selectedRows = this.state.rows.map((row, index) => { return index });
+    }else if( (typeof selrows === 'string') && selrows === 'none'){
+      _selectedRows = [];
+    }else{
+      _selectedRows = selrows;
+    }
+
+    this.setState({selectedRows: _selectedRows});
+  }
+
   render() {
     const styles = this.styles;
-    const {openRepoTree} = this.state;
+
+    const { openRepoTree, showMoreFilter, selectedRows, rows } = this.state;
+
+    const filterStyle = showMoreFilter ? styles.topbar : Object.assign({}, styles.topbar, {display: 'none'});
+
+    const hasSelected = selectedRows && selectedRows.length > 0;
+
+    const rowEls = rows.map((row, index) => {
+      let filterRows = selectedRows.filter( i => (i === index) );
+      return (
+              <TableRow  key={`tr-${row.id}`} selected={ filterRows && filterRows.length > 0}>
+                <TableRowColumn>{row.id}</TableRowColumn>
+                <TableRowColumn>{row.name}</TableRowColumn>
+                <TableRowColumn>{row.label}</TableRowColumn>
+              </TableRow>
+      );
+    });
 
     return (
 
@@ -222,46 +306,50 @@ class WGroupRepoPage extends React.Component {
               style={styles.popover}>
               <TreeList nodeIcon={this.nodeIcon} nodes={nodes} muiTheme={this.props.muiTheme}/>
             </Popover>
-            <Breadcrumb items={links} onJumpClick={this.handJumpLink} style={{verticalAlign:'top', display:'inline-block'}} muiTheme={this.props.muiTheme}/>
-            <IconButton onTouchTap={ console.log('--')} style={{float:'right'}} iconStyle={styles.iconStyle}>
-              <NaviExpandMore/>
+            <Breadcrumb items={links} onJumpClick={this.handleJumpLink} style={{verticalAlign:'top', display:'inline-block'}} muiTheme={this.props.muiTheme}/>
+            <IconButton onTouchTap={ this.handleShowMoreFilter } style={{float:'right'}} iconStyle={styles.iconStyle}>
+              { showMoreFilter ? <NaviExpandLess/> : <NaviExpandMore/> }
             </IconButton>
           </div>
-
-          <Table multiSelectable={true}>
-            <TableHeader>
+          <div style={ filterStyle }>
+            <TextField style={styles.filterSearch} hintText="File or folder name"/>
+            <SelectField style={styles.filterType} hintText='Format'>
+              <MenuItem value={ 'doc' } primaryText='Office Word' />
+              <MenuItem value={ 'xls' } primaryText='Office Excel' />
+              <MenuItem value={ 'pdf' } primaryText='PDF' />
+              <MenuItem value={ 'ALL' } primaryText='All' />
+            </SelectField>
+            <SelectField style={styles.filterClass} hintText='Classification'>
+              <MenuItem value={ 'OPEN' } primaryText='Unclassified' />
+              <MenuItem value={ 'CLOSE' } primaryText='Top secret' />
+              <MenuItem value={ 'FULL' } primaryText='Secret' />
+              <MenuItem value={ 'FULL' } primaryText='Credential' />
+              <MenuItem value={ 'ALL' } primaryText='All' />
+            </SelectField>
+            <div style={ styles.spacer } />
+            <div style={{width:96}}>
+              <IconButton iconStyle={styles.iconStyle}><ActSearch/></IconButton>
+              <IconButton iconStyle={styles.iconStyle}><CtntClear/></IconButton>
+            </div>
+          </div>
+          <div>
+            <RaisedButton label='Clear' style={ { margin: 4 } } onTouchTap={ this.handleClear } disabled={hasSelected} />
+            <RaisedButton label='Clear' style={ { margin: 4 } } onTouchTap={ this.handleClear } />
+            <RaisedButton label='Clear' style={ { margin: 4 } } onTouchTap={ this.handleClear } />
+            <Divider style={{marginTop:10}}/>
+          </div>
+          <Table multiSelectable={true}
+            selectable= {true}
+            onRowSelection={this.handleRowSelection}>
+            <TableHeader enableSelectAll={true}>
               <TableRow>
-                //<TableHeaderColumn>ID</TableHeaderColumn>
-                //<TableHeaderColumn>Name</TableHeaderColumn>
-                //<TableHeaderColumn>Status</TableHeaderColumn>
+                <TableHeaderColumn>ID</TableHeaderColumn>
+                <TableHeaderColumn>Name</TableHeaderColumn>
+                <TableHeaderColumn>Status</TableHeaderColumn>
               </TableRow>
             </TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableRowColumn>1</TableRowColumn>
-                <TableRowColumn>John Smith</TableRowColumn>
-                <TableRowColumn>Employed</TableRowColumn>
-              </TableRow>
-              <TableRow>
-                <TableRowColumn>2</TableRowColumn>
-                <TableRowColumn>Randal White</TableRowColumn>
-                <TableRowColumn>Unemployed</TableRowColumn>
-              </TableRow>
-              <TableRow>
-                <TableRowColumn>3</TableRowColumn>
-                <TableRowColumn>Stephanie Sanders</TableRowColumn>
-                <TableRowColumn>Employed</TableRowColumn>
-              </TableRow>
-              <TableRow>
-                <TableRowColumn>4</TableRowColumn>
-                <TableRowColumn>Steve Brown</TableRowColumn>
-                <TableRowColumn>Employed</TableRowColumn>
-              </TableRow>
-              <TableRow>
-                <TableRowColumn>5</TableRowColumn>
-                <TableRowColumn>Christopher Nolan</TableRowColumn>
-                <TableRowColumn>Unemployed</TableRowColumn>
-              </TableRow>
+            <TableBody deselectOnClickaway={false}>
+              {rowEls}
             </TableBody>
           </Table>
         </div>

15d62e51e3f8f0a36672078f1ae589113a663df9
despird-zh <despird.zhang@yahoo.com>
2017-05-22 16:41:12 +0800

    update

--- source/js/views/wgroup/WGroupBulletinPage.jsx
+++ source/js/views/wgroup/WGroupBulletinPage.jsx
@@ -8,6 +8,7 @@ import PageStyle from './WGroupBulletinPage.scss';
 
 function getStyles(muiTheme) {
   const { baseTheme } = muiTheme;
+  console.log(baseTheme.palette)
   return {
     root: {
       display: 'flex',
@@ -34,27 +35,41 @@ function getStyles(muiTheme) {
     },
     chatRow: {
       padding: 0,
-      margin:'0 0 10px 0',
+      margin:0,
       overflow: 'hidden',
-      display: 'flex'
+      display: 'flex',
+      fontWeight: 500,
     },
     chatMsg: {
       flex:1,
       display: 'block',
       position: 'relative',
+      paddingBottom: 5,
     },
-    chatSelf: {
+    chatOther: {
       display: 'block',
       float: 'right',
       padding: 10,
-      marginRight: 20,
+      marginRight: 15,
       minWidth: 100,
       position: 'relative',
       fontSize: 14,
       borderRadius: 3,
-      boxShadow: '0 2px 6px rgba(0, 0, 0, 0.3)'
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.3)'
+    },
+    chatSelf: {
+      display: 'block',
+      float: 'left',
+      padding: 10,
+      marginLeft: 15,
+      minWidth: 100,
+      position: 'relative',
+      fontSize: 14,
+      borderRadius: 3,
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.3)'
     },
     chatSide: {
+      padding: 10,
       flexBasis: 60,
       flexShrink: 0,
       flexGrow: 0,
@@ -80,26 +95,50 @@ class WGroupBulletinPage extends React.Component {
 
     return (
       <div style={ Object.assign(this.props.style, styles.root) }>
-        <div className={'chat'}>
+        <div className={'chat'} style={styles.chat}>
           <ul style={ styles.chatList }>
             <li style={ styles.chatRow } className={'other'}>
               <div style={ styles.chatSide }>
               </div>
               <div style={ styles.chatMsg } >
-                <div style={ styles.chatSelf } className={'message'}>
+                <div  className={'message-bar clearfix'}>
+                  <time>13:23</time>
+                </div>
+                <div style={ styles.chatOther } className={'message'}>
                   <p>
                     Itaque quod et dolore accusantium. Labore aut similique ab voluptas rerum quia. Reprehenderit voluptas doloribus ut nam tenetur ipsam.
                   </p>
                 </div>
               </div>
               <div style={ styles.chatSide }>
-                <Avatar src="assets/img/kerem-128.jpg" size={40} style={{ marginLeft:5}} />
+                <Avatar src="assets/img/kerem-128.jpg" size={40} />
               </div>
             </li>
             <li style={ styles.chatRow } className={'other'}>
               <div style={ styles.chatSide }>
               </div>
               <div style={ styles.chatMsg } className={'clearfix'}>
+                <div className={'message-bar clearfix'}>
+                  <time>13:23</time>
+                </div>
+                <div style={ styles.chatOther } className={'message'}>
+                  <p>
+                    Itaque quod et dolore accusantium.
+                  </p>
+                </div>
+              </div>
+              <div style={ styles.chatSide }>
+                <Avatar src="assets/img/kerem-128.jpg" size={40}/>
+              </div>
+            </li>
+            <li style={ styles.chatRow } className={'you'}>
+              <div style={ styles.chatSide }>
+              <Avatar src="assets/img/kerem-128.jpg" size={40} />
+              </div>
+              <div style={ styles.chatMsg } className={'clearfix'}>
+                <div  className={'message-bar clearfix'}>
+                  <time style={{float: 'none'}}>13:23</time>
+                </div>
                 <div style={ styles.chatSelf } className={'message'}>
                   <p>
                     Itaque quod et dolore accusantium.
@@ -107,97 +146,27 @@ class WGroupBulletinPage extends React.Component {
                 </div>
               </div>
               <div style={ styles.chatSide }>
-                <Avatar src="assets/img/kerem-128.jpg" size={40} style={{ marginLeft:5}} />
+              </div>
+            </li>
+            <li style={ styles.chatRow } className={'you'}>
+              <div style={ styles.chatSide }>
+              <Avatar src="assets/img/kerem-128.jpg" size={40} />
+              </div>
+              <div style={ styles.chatMsg } className={'clearfix'}>
+                <div  className={'message-bar clearfix'}>
+                  <time style={{float: 'none'}}>13:23</time>
+                </div>
+                <div style={ styles.chatSelf } className={'message'}>
+                  <p>
+                   Itaque quod et dolore accusantium. Labore aut similique ab voluptas rerum quia. Reprehenderit voluptas doloribus ut nam tenetur ipsam.
+                  </p>
+                </div>
+              </div>
+              <div style={ styles.chatSide }>
               </div>
             </li>
           </ul>
-          <ul>
 
-          <li className={'other'}>
-            <a className={'user'} href="#"><img alt="" src="https://s3.amazonaws.com/uifaces/faces/twitter/toffeenutdesign/128.jpg"/></a>
-            <div className={'date'}>
-              2 minutes ago
-            </div>
-            <div className={'message'}>
-              <div className={'hider'}>
-                <span>Click to read</span>
-              </div>
-              <p>
-                Itaque quod et dolore accusantium. Labore aut similique ab voluptas rerum quia. Reprehenderit voluptas doloribus ut nam tenetur ipsam.
-              </p>
-            </div>
-          </li>
-          <li className={'other'}>
-            <a className={'user'} href="#"><img alt="" src="https://s3.amazonaws.com/uifaces/faces/twitter/toffeenutdesign/128.jpg"/></a>
-            <div className={'date'}>
-              5 minutes ago
-            </div>
-            <div className={'message'}>
-              <div className={'hider'}>
-                <span>Click to read</span>
-              </div>
-              <p>
-                Modi ratione aliquid non. Et porro deserunt illum sed velit necessitatibus. Quis fuga et et fugit consequuntur. Et veritatis fugiat veniam pariatur maxime iusto aperiam.
-              </p>
-            </div>
-          </li>
-          <li className={'you'}>
-            <a className={'user'} href="#"><img alt="" src="https://s3.amazonaws.com/uifaces/faces/twitter/igorgarybaldi/128.jpg"/></a>
-            <div className={'date'}>
-              7 minutes ago
-            </div>
-            <div className={'message'}>
-              <div className={'hider'}>
-                <span>Click to read</span>
-              </div>
-              <p>
-                Provident impedit atque nemo culpa et modi molestiae. Error non dolorum voluptas non a. Molestiae et nobis nisi sed.
-              </p>
-            </div>
-          </li>
-          <li className={'other'}>
-            <a className={'user'} href="#"><img alt="" src="https://s3.amazonaws.com/uifaces/faces/twitter/toffeenutdesign/128.jpg"/></a>
-            <div className={'date'}>
-              8 minutes ago
-            </div>
-            <div className={'message'}>
-              <div className={'hider'}>
-                <span>Click to read</span>
-              </div>
-              <p>
-                Id vel ducimus perferendis fuga excepturi nulla. Dolores dolores amet et laborum facilis. Officia magni ut non autem et qui incidunt. Qui similique fugit vero porro qui cupiditate.
-              </p>
-            </div>
-          </li>
-          <li className={'you'}>
-            <a className={'user'} href="#"><img alt="" src="https://s3.amazonaws.com/uifaces/faces/twitter/igorgarybaldi/128.jpg"/></a>
-            <div className={'date'}>
-              10 minutes ago
-            </div>
-            <div className={'message'}>
-              <div className={'hider'}>
-                <span>Click to read</span>
-              </div>
-              <p>
-                Provident impedit atque nemo culpa et modi molestiae. Error non dolorum voluptas non a. Molestiae et nobis nisi sed.
-              </p>
-            </div>
-          </li>
-          <li className={'you'}>
-            <a className={'user'} href="#"><img alt="" src="https://s3.amazonaws.com/uifaces/faces/twitter/igorgarybaldi/128.jpg"/></a>
-            <div className={'date'}>
-              10 minutes ago
-            </div>
-            <div className={'message'}>
-              <div className={'hider'}>
-                <span>Click to read</span>
-              </div>
-              <p>
-                Est ut at eum sed perferendis ea hic. Tempora perspiciatis magnam aspernatur explicabo ea. Sint atque quod.
-              </p>
-            </div>
-          </li>
-        </ul>
         </div>
         <WGroupProfileLite muiTheme={ this.props.muiTheme } style={ styles.rightPanel } />
       </div>

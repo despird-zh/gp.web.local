f3fc058f5abf4d67fb079f74f7973fdb97b7c982
despird-zh <despird.zhang@yahoo.com>
2017-05-25 20:07:03 +0800

    update

--- source/js/views/component/TextEditor/HtmlEditor.jsx
+++ source/js/views/component/TextEditor/HtmlEditor.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import EditAttatchFile from 'material-ui/svg-icons/editor/attatch-file';
-
+import EditInsertPhoto from 'material-ui/svg-icons/editor/insert-photo';
 import { Editor, EditorState, RichUtils } from 'draft-js';
 import EditorStyle from './HtmlEditor.scss';
 
